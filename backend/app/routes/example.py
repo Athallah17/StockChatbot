b783edfe9fa62ea8a85e71798f5491c125651fcf
AthallahK <ayaristyan255@mandirisekuritas.co.id>
2025-03-07 17:47:30 +0700

    Change API Test Message

--- backend/app/routes/example.py
+++ backend/app/routes/example.py
@@ -4,4 +4,4 @@ router = APIRouter()
 
 @router.get("/example")
 def read_example():
-    return {"message": "This is an example endpoint from the backend with RESTAPI, example.py!"}
+    return {"message": "Hello This is Example Message from Endpoint! This message was created using REST API"}
